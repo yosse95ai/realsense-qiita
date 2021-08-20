b299020f8205e519504cd34411a4d6bda01fbf9b
yosse95ai <surface-20170218@outlook.jp>
2021-08-21 01:24:18 +0900

    06の記事用にアップデート

--- qiita.cpp
+++ qiita.cpp
@@ -1,10 +1,35 @@
-﻿#include <iostream>
+﻿#pragma once
+#include <iostream>
 #include "rs2Class.h"
+
+/// <summary>
+/// デバイス情報を表示する
+/// </summary>
+/// <param name="info">デバイスの情報</param>
+void showDeviceInfo(RsCamera camera)
+{
+	DeviceInfo info = camera.getDeviceInfo();
+	std::cout
+		<< "** Device info **" << std::endl
+		<< "  NAME         : " << info["NAME"] << std::endl
+		<< "  PRODUCT LINE : " << info["PRODUCT_LINE"] << std::endl
+		<< "  PRODUCT ID   : " << info["PRODUCT_ID"] << std::endl
+		<< "  SERIAL NUMBER       : " << info["SERIAL_NUMBER"] << std::endl
+		<< "  ASIC SERIAL NUMBER  : " << info["ASIC_SERIAL_NUMBER"] << std::endl
+		<< "  FIRMWARE UPDATE ID  : " << info["FIRMWARE_UPDATE_ID"] << std::endl
+		<< "  FIRMWARE_VERSION    : " << info["FIRMWARE_VERSION"] << std::endl
+		<< "  ADVANCED MODE       : " << info["ADVANCED_MODE"] << std::endl
+		<< "  CAMERA LOCKED       : " << info["CAMERA_LOCKED"] << std::endl
+		<< "  DEBUG OP CODE       : " << info["DEBUG_OP_CODE"] << std::endl
+		<< "  USB TYPE DESCRIPTOR : " << info["USB_TYPE_DESCRIPTOR"] << std::endl
+		<< "  RECOMMENDED FIRMWARE VERSION : " << info["RECOMMENDED_FIRMWARE_VERSION"] << std::endl
+		<< "  PHYSICAL_PORT :  " << info["PHYSICAL_PORT"] << std::endl;
+}
 
 int main()
 try {
 
-	// pipeline config
+	// Pipeline config
 	rs2::config cfg;
 	cfg.enable_stream(RS2_STREAM_COLOR, WIDTH, HEIGHT, RS2_FORMAT_BGR8, FPS);	// Color
 	cfg.enable_stream(RS2_STREAM_DEPTH, WIDTH, HEIGHT, RS2_FORMAT_Z16, FPS);	// Depth
@@ -12,14 +37,20 @@ try {
 	// RsCamera instance
 	RsCamera camera(cfg);
 
+	// Show device info
+	//showDeviceInfo(camera);
+	for (auto info : camera.getDeviceInfoVer2()) {
+		std::cout << info.first << " :  " << info.second << std::endl;
+	}
+
 	// Align color stream to depth stream
-	rs2::align align(RS2_STREAM_DEPTH);
+	rs2::align align(RS2_STREAM_COLOR);
 
 	// Display image
 	cv::Mat image(cv::Size(2 * WIDTH, HEIGHT), CV_8UC3);
 
 
-	while (true)
+	while (false)
 	{
 		// カラーフレームを取得
 		//camera.getColorFrame(image);
@@ -31,7 +62,7 @@ try {
 		camera.getComboFrame(image, align);
 
 		// 画像を表示
-		cv::imshow("DEPTH_FRAME", image);
+		cv::imshow("COMBO_FRAME", image);
 
 		// ESCで終了
 		if (cv::waitKey(10) == 27) {
--- rs2Class.h
+++ rs2Class.h
@@ -1,10 +1,13 @@
 #pragma once
+#include <iostream>
 #include "librealsense2/rs.hpp"
 #include <opencv2/opencv.hpp>
 
 #define WIDTH 640
 #define HEIGHT 480
 #define FPS 30
+
+typedef std::map<std::string, std::string> DeviceInfo;
 
 /// <summary>
 /// D455カメラ操作クラス
@@ -13,22 +16,61 @@ class RsCamera
 {
 public:
 	// コンストラクタ
-	RsCamera() { pipe.start(); }
-	RsCamera(rs2::config cfg) { pipe.start(cfg); }
+	RsCamera();
+	RsCamera(rs2::config cfg);
 
 	// デストラクタ
-	~RsCamera(){ pipe.stop(); }
+	~RsCamera() { pipe.stop(); }
+
 
 	float getCenterDistance();
 	void getColorFrame(cv::Mat& color_image);
 	void getDepthFrame(cv::Mat& depth_image);
 	void getComboFrame(cv::Mat& combo_image, rs2::align align);
+	DeviceInfo getDeviceInfo();
+	DeviceInfo getDeviceInfoVer2();
 
 private:
+	bool isConnectedDevices();
+
 	rs2::pipeline pipe;
 	rs2::frameset frames;
 	rs2::colorizer color_map;
+	rs2::context ctx;
+	rs2::device_list devices;
 };
+
+/*********************************************************
+*
+* Public functions
+*
+**********************************************************/
+/// <summary>
+/// コンストラクタ
+/// </summary>
+RsCamera::RsCamera()
+{
+	if (isConnectedDevices()) {
+		pipe.start();
+	}
+	else {
+		throw std::runtime_error("ERR: No cameras are connected.");
+	}
+}
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+/// <param name="cfg">設定情報</param>
+RsCamera::RsCamera(rs2::config cfg)
+{
+	if (isConnectedDevices()) {
+		pipe.start(cfg);
+	}
+	else {
+		throw std::runtime_error("ERR: No cameras are connected.");
+	}
+}
 
 /// <summary>
 /// 画面中央の深度を取得
@@ -51,8 +93,8 @@ float RsCamera::getCenterDistance()
 /// <summary>
 /// カラーフレームを取得する
 /// </summary>
-/// <param name="color_image">cv::Mat</param>
-void RsCamera::getColorFrame(cv::Mat& color_image) 
+/// <param name="color_image">フレーム出力先</param>
+void RsCamera::getColorFrame(cv::Mat& color_image)
 {
 	rs2::frameset frames = pipe.wait_for_frames();
 	rs2::video_frame color_frame = frames.get_color_frame();
@@ -65,7 +107,7 @@ void RsCamera::getColorFrame(cv::Mat& color_image)
 /// <summary>
 /// 深度フレームを取得する
 /// </summary>
-/// <param name="depth_image">cv::Mat</param>
+/// <param name="depth_image">フレーム出力先</param>
 void RsCamera::getDepthFrame(cv::Mat& depth_image)
 {
 	rs2::frameset frames = pipe.wait_for_frames();
@@ -76,6 +118,11 @@ void RsCamera::getDepthFrame(cv::Mat& depth_image)
 	image.copyTo(depth_image);
 }
 
+/// <summary>
+/// カラーフレームとDepthフレームを取得する
+/// </summary>
+/// <param name="combo_image">フレーム出力先</param>
+/// <param name="align">アラインメントの設定</param>
 void RsCamera::getComboFrame(cv::Mat& combo_image, rs2::align align)
 {
 	rs2::frameset frames = pipe.wait_for_frames();
@@ -91,4 +138,66 @@ void RsCamera::getComboFrame(cv::Mat& combo_image, rs2::align align)
 	color_image.copyTo(color_positon);
 	cv::Mat depth_positon(combo_image, cv::Rect(WIDTH, 0, WIDTH, HEIGHT));
 	depth_image.copyTo(depth_positon);
+}
+
+/// <summary>
+/// デバイスの情報を連想配列で取得する
+/// </summary>
+/// <returns>デバイスの情報の連想配列</returns>
+DeviceInfo RsCamera::getDeviceInfo()
+{
+	rs2::device device = devices[0];
+	DeviceInfo dev_info;
+
+	dev_info["NAME"] = device.get_info(RS2_CAMERA_INFO_NAME);
+	dev_info["ADVANCED_MODE"] = device.get_info(RS2_CAMERA_INFO_ADVANCED_MODE);
+	dev_info["ASIC_SERIAL_NUMBER"] = device.get_info(RS2_CAMERA_INFO_ASIC_SERIAL_NUMBER);
+	dev_info["CAMERA_LOCKED"] = device.get_info(RS2_CAMERA_INFO_CAMERA_LOCKED);
+	dev_info["DEBUG_OP_CODE"] = device.get_info(RS2_CAMERA_INFO_DEBUG_OP_CODE);
+	dev_info["FIRMWARE_UPDATE_ID"] = device.get_info(RS2_CAMERA_INFO_FIRMWARE_UPDATE_ID);
+	dev_info["FIRMWARE_VERSION"] = device.get_info(RS2_CAMERA_INFO_FIRMWARE_VERSION);
+	dev_info["PRODUCT_LINE"] = device.get_info(RS2_CAMERA_INFO_PRODUCT_LINE);
+	dev_info["PRODUCT_ID"] = device.get_info(RS2_CAMERA_INFO_PRODUCT_ID);
+	dev_info["PHYSICAL_PORT"] = device.get_info(RS2_CAMERA_INFO_PHYSICAL_PORT);
+	dev_info["RECOMMENDED_FIRMWARE_VERSION"] = device.get_info(RS2_CAMERA_INFO_RECOMMENDED_FIRMWARE_VERSION);
+	dev_info["SERIAL_NUMBER"] = device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER);
+	dev_info["USB_TYPE_DESCRIPTOR"] = device.get_info(RS2_CAMERA_INFO_USB_TYPE_DESCRIPTOR);
+
+	return dev_info;
+}
+
+/// <summary>
+/// RS2_CAMERA_INFO_COUNTを使用した例
+/// </summary>
+/// <returns>デバイスの情報の連想配列</returns>
+DeviceInfo RsCamera::getDeviceInfoVer2()
+{
+	rs2::device device = devices[0];
+	DeviceInfo dev_info;
+	for (int i = 0; i < RS2_CAMERA_INFO_COUNT; ++i) {
+		rs2_camera_info info = (rs2_camera_info)i;
+
+		if (device.supports(info)) {
+			auto info_key = rs2_camera_info_to_string(info);
+			dev_info[info_key] = device.get_info(info);
+		}
+	}
+	return dev_info;
+}
+
+/*********************************************************
+*
+* Private functions
+*
+**********************************************************/
+/// <summary>
+/// 接続デバイス情報を更新
+/// </summary>
+/// <returns>T:接続あり，F:接続無し</returns>
+bool RsCamera::isConnectedDevices() {
+	devices = ctx.query_devices();
+	if (!devices.size()) { 
+		return false;
+	}
+	return true;
 }
